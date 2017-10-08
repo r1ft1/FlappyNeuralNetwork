18a592ffdd999089a629c954d142a7852ea19b3b
r1ft1 <jamessundaram8@gmail.com>
2017-10-08 17:49:30 +0100

    Beginning work on Neural Network

--- Assets/Scripts/Perceptron.cs
+++ Assets/Scripts/Perceptron.cs
@@ -2,23 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Perceptron {
+public class Perceptron : MonoBehaviour {
 
-	// Use this for initialization
+	public Bird bird;
+
+	float[] inputs = new float[2];
+	float[] weights = new float[2];
+	float sum = 0;
+
 	void Start () {
-		float[] inputs = new float[3];
-		float[] weights = new float[3];
-		float sum;
-
 		for (int i = 0; i < weights.Length; i++) {
 			weights [i] = Random.Range (-1.0f,1.0f);
 		}
 
 	}
 
-	public double activateSigmoid(float sum)
+
+	void FixedUpdate()
+	{
+		inputs [0] = bird.birdHeight;
+		inputs [1] = bird.nextPipeHeight;
+		feedForward (inputs);
+	}
+
+	float feedForward(float[] inputs)
+	{
+		for (int i = 0; i < weights.Length; i++) {
+			sum += inputs [i] * weights [i];
+		}
+		return activateSigmoid(sum);
+	}
+
+
+	public float activateSigmoid(float sum)
 	{
 		return 1 / (1 + Mathf.Exp (-sum));
 	}
 
-}
+}                                                                
